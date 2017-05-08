d51970037806861f797b881c6d945dfa085f0a33
a15ramha <ram_gt94@live.se>
2017-05-08 11:11:38 +0200

    Support for tables in showdoc

--- Shared/markdown.js
+++ Shared/markdown.js
@@ -209,7 +209,7 @@ function handleUnorderedList(currentLine, prevLine, nextLine) {
     if(currentLineIndentation < nextLineIndentation) { 
     	markdown += "<li>";
     	markdown +=  value;
-    	markdown += "<ul>" 
+    	markdown += "<ul>";
     }
     // Close sublists
     else if(currentLineIndentation > nextLineIndentation) { 
@@ -250,7 +250,7 @@ function handleOrderedList(currentLine, prevLine, nextLine) {
     	markdown +=  value;
 
     	// open sublist
-    	markdown += "<ol>" 
+    	markdown += "<ol>";
     }
     // Close sublists
     else if(currentLineIndentation > nextLineIndentation) { 
@@ -312,7 +312,7 @@ function handleTable(currentLine, prevLine, nextLine) {
         }
         // handle table row
         else {
-            markdown += "<tr style=''>"
+            markdown += "<tr style=''>";
             for(var i = 0; i < columns.length; i++) {
                 var alignment = "";
 
